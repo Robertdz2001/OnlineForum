7d01be8dae4344b81f5494d83ba06b4fbdb6997f
Robertdz2001 <robertdz0@onet.pl>
2022-11-01 13:09:01 +0100

    Add user and role entities. Add ForumDbContext

---
+++ Forum/Forum/Entities/ForumDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Entities
+{
+    public class ForumDbContext : DbContext 
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options): base(options){}
+
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.UserName)
+                .HasMaxLength(25)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
+               .Property(r => r.Name)
+               .IsRequired();
+        }
+    }
+}
